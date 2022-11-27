fb48f9cf7171578ee0453d626958996116f025e1
miki-l <miki.l@binance.com>
2022-11-27 14:18:40 +0800

    Add TG bot

---
+++ index.js
@@ -0,0 +1,36 @@
+const TelegramBot = require('node-telegram-bot-api');
+const schedule = require('node-schedule')
+const token = '5870266794:AAHWa4yA_KYsVRtir_OHEYX9nX1MvtpQ4w8';
+
+let bot = new TelegramBot(token, { polling: true });
+let job;
+
+
+const dayMap = new Map();
+
+bot.onText(/^\/add/, function(msg, match) {
+    bot.sendMessage(msg.chat.id, 'Please input the date you want to count down to in format YYYY-MM-DD (e.g. 2023-12-25)');
+});
+
+bot.onText(/^\d{4}-\d{2}-\d{2}$/, (msg) => {
+    const day = msg.text
+    bot.sendMessage(msg.chat.id, "OK, the cont down date is " + msg.text);
+    dayMap.set(msg.chat.id, day)
+    job = schedule.scheduleJob('0 * * * * *', () => {
+        let today = new Date().getTime()
+        let endDay = new Date(day).getTime()
+        let differenceDay = Math.ceil((endDay-today) / (1000 * 3600 * 24));
+        bot.sendMessage(msg.chat.id, differenceDay + ' day left \u{1F389}');
+    });
+});
+
+
+bot.onText(/\/start/, message => {
+    bot.sendMessage(message.chat.id, "Hola! Click on Add countdown or type /add to add \u{1F60D}.");
+});
+
+bot.onText(/\/stop/, message => {
+    if (job) {
+        job.cancel()
+    }
+});
