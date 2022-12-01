64d403bc12247c66914ee1e1e6e0d087369d47ef
miki-l <miki.l@binance.com>
2022-12-01 10:29:40 +0800

    Update send msg, hide token, add gitignore

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const TelegramBot = require('node-telegram-bot-api');
 const schedule = require('node-schedule')
-const token = '5870266794:AAHWa4yA_KYsVRtir_OHEYX9nX1MvtpQ4w8';
+const token = process.env.TOKEN;
 
 let bot = new TelegramBot(token, { polling: true });
 let job;
-
 
 const dayMap = new Map();
 
@@ -13,16 +12,17 @@ function dayDifference(day, msg) {
     let today = new Date().getTime();
     let endDay = new Date(day).getTime();
     let differenceDay = Math.ceil((endDay - today) / (1000 * 3600 * 24));
-    bot.sendMessage(msg.chat.id, differenceDay + ' day left \u{1F389}');
+    bot.sendMessage(msg.chat.id, differenceDay + ' day left to ' + msg.text + '\u{1F389}');
 }
 
 bot.onText(/^\/\d{4}-\d{2}-\d{2}$/, (msg) => {
     msg.text = msg.text.split('/')[1]
     const day = msg.text
-    bot.sendMessage(msg.chat.id, "OK, the cont down date is " + msg.text);
+    bot.sendMessage(msg.chat.id, "OK, the count down date is " + msg.text);
     dayDifference(day, msg);
     dayMap.set(msg.chat.id, day)
-    job = schedule.scheduleJob('0 0 0 * * *', () => {
+    console.log(dayMap)
+    job = schedule.scheduleJob('0 0 16 * * *', () => {
         dayDifference(day, msg);
     });
 });
@@ -35,7 +35,7 @@ bot.onText(/\/start/, message => {
     bot.sendMessage(message.chat.id, "Hola! Click on Add countdown or type /add to add \u{1F60D}.");
 });
 
-bot.onText(/\/stop/, message => {
+bot.onText(/\/stop/, () => {
     if (job) {
         job.cancel()
     }
