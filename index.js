1019d4240dc25aca02888d4026fe3fad4e7518ec
rain.z <rain.z@binance.com>
2023-01-19 21:05:54 +0800

    1、delete sensitive information; 2、fix eslint rules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const http = require('http')
-const TelegramBot = require('node-telegram-bot-api')
-const schedule = require('node-schedule')
+const http = require('http');
+const TelegramBot = require('node-telegram-bot-api');
+const schedule = require('node-schedule');
 
 // The following string can be changed to a template
 
@@ -9,19 +9,19 @@ class CustomBot {
     constructor(token) {
 
         if (!token) {
-            throw new Error('Token must not be empty')
+            throw new Error('Token must not be empty');
         }
 
         const options = {
             polling: true
-        }
+        };
 
         // The structure of dayMap is key=fromId-chatId, value=daySet
-        this.dayMap = new Map()
+        this.dayMap = new Map();
         // The structure of dayMap is key=fromId-chatId value=jobObj
-        this.jobMap = new Map()
-        this.bot = new TelegramBot(token, options)
-        this.listenCommand()
+        this.jobMap = new Map();
+        this.bot = new TelegramBot(token, options);
+        this.listenCommand();
     }
 
     /**
@@ -30,43 +30,43 @@ class CustomBot {
     listenCommand() {
 
         // If there are new commands and corresponding operations to be added, you can refer to the following format
-        this.bot.onText(/^\/add/, this.onAddCommand.bind(this))
-        this.bot.onText(/\/start/, this.onStartCommand.bind(this))
-        this.bot.onText(/\/stop/, this.onStopCommand.bind(this))
-        this.bot.onText(/[\s\S]*/, this.onCommonStr.bind(this))
+        this.bot.onText(/^\/add/, this.onAddCommand.bind(this));
+        this.bot.onText(/\/start/, this.onStartCommand.bind(this));
+        this.bot.onText(/\/stop/, this.onStopCommand.bind(this));
+        this.bot.onText(/[\s\S]*/, this.onCommonStr.bind(this));
 
-        this.bot.on('polling_error', console.error)
+        this.bot.on('polling_error', console.error);
     }
 
     // listen handler start
 
     // Listening for add commands
     onAddCommand(msg) {
-        const cmdAndDate = msg.text.split(' ')
+        const cmdAndDate = msg.text.split(' ');
         if (cmdAndDate.length !== 2) {
-            this.sendMessage(msg.chat.id, 'Please enter the specified format /add date, e.g. /add 2023-12-25')
-            return
+            this.sendMessage(msg.chat.id, 'Please enter the specified format /add date, e.g. /add 2023-12-25');
+            return;
         }
 
         // Send success message
-        const date = cmdAndDate[1]
-        this.sendMessage(msg.chat.id, `OK, the count down date is ${date}`)
+        const date = cmdAndDate[1];
+        this.sendMessage(msg.chat.id, `OK, the count down date is ${date}`);
 
-        const diffDay = this.dayDifference(date)
-        this.sendMessage(msg.chat.id, '\u{1F60D} ' + diffDay + ' days left to ' + date + '\u{1F389}')
+        const diffDay = this.dayDifference(date);
+        this.sendMessage(msg.chat.id, '\u{1F60D} ' + diffDay + ' days left to ' + date + '\u{1F389}');
 
-        this.updateDayMap(msg, date)
-        this.updateJob(msg)
+        this.updateDayMap(msg, date);
+        this.updateJob(msg);
     }
 
     // Listening for start commands
     onStartCommand(msg) {
-        this.sendMessage(msg.chat.id, 'Hola! Click on Add countdown or type /add 2023-12-25 to add \u{1F60D}.')
+        this.sendMessage(msg.chat.id, 'Hola! Click on Add countdown or type /add 2023-12-25 to add \u{1F60D}.');
     }
 
     // Listening for stop commands
     onStopCommand(msg) {
-        this.stopJob(msg)
+        this.stopJob(msg);
     }
 
     // Listening for generic string content
@@ -83,14 +83,14 @@ class CustomBot {
      * @param date
      */
     updateDayMap(msg, date) {
-        const key = `${msg.from.id}-${msg.chat.id}`
-        let dateSet = this.dayMap.get(key)
+        const key = `${msg.from.id}-${msg.chat.id}`;
+        let dateSet = this.dayMap.get(key);
 
         if (!dateSet) {
-            dateSet = new Set()
+            dateSet = new Set();
         }
-        dateSet.add(date)
-        this.dayMap.set(key, dateSet)
+        dateSet.add(date);
+        this.dayMap.set(key, dateSet);
     }
 
     /**
@@ -102,24 +102,24 @@ class CustomBot {
      */
     updateJob(msg) {
         if (!msg) {
-            return
+            return;
         }
 
-        this.stopJob(msg)
+        this.stopJob(msg);
 
-        const that = this
+        const that = this;
         const job = schedule.scheduleJob('0 0 16 * * *', () => {
-            const dateSet = that.dayMap.get(key)
+            const dateSet = that.dayMap.get(key);
             if (dateSet) {
                 dateSet.forEach(date => {
-                    const jobDiffDay = that.dayDifference(date)
-                    that.sendMessage(key.split('-')[1], jobDiffDay + ' days left to ' + msg.text + '\u{1F389}')
-                })
+                    const jobDiffDay = that.dayDifference(date);
+                    that.sendMessage(key.split('-')[1], jobDiffDay + ' days left to ' + msg.text + '\u{1F389}');
+                });
             }
-        })
+        });
 
-        const key = `${msg.from.id}-${msg.chat.id}`
-        this.jobMap.set(key, job)
+        const key = `${msg.from.id}-${msg.chat.id}`;
+        this.jobMap.set(key, job);
     }
 
     /**
@@ -127,12 +127,12 @@ class CustomBot {
      * @param msg
      */
     stopJob(msg) {
-        const key = `${msg.from.id}-${msg.chat.id}`
-        let job = this.jobMap.get(key)
+        const key = `${msg.from.id}-${msg.chat.id}`;
+        let job = this.jobMap.get(key);
 
         if (job) {
-            console.log(`Stop ${key} job`)
-            job.cancel()
+            console.log(`Stop ${key} job`);
+            job.cancel();
         }
     }
 
@@ -142,27 +142,25 @@ class CustomBot {
      * @returns Date difference
      */
     dayDifference(day) {
-        let today = new Date().getTime()
-        let endDay = new Date(day).getTime()
-        return Math.ceil((endDay - today) / (1000 * 3600 * 24))
+        let today = new Date().getTime();
+        let endDay = new Date(day).getTime();
+        return Math.ceil((endDay - today) / (1000 * 3600 * 24));
     }
 
     sendMessage(chatId, msgStr) {
-        this.bot.sendMessage(chatId, msgStr)
+        this.bot.sendMessage(chatId, msgStr);
     }
 }
 
 const server = http.createServer((_, res) => {
-    res.writeHead(200)
-    res.end('OK')
-})
+    res.writeHead(200);
+    res.end('OK');
+});
 
 server.listen(8080, () => {
-    new CustomBot(process.env.TOKEN || '')
-    console.log('Server started')
-})
+    new CustomBot(process.env.TOKEN || '');
+    console.log('Server started');
+});
 
-process.on('SIGTERM', () => server.close())
-
-// TOKEN=5870266794:AAHWa4yA_KYsVRtir_OHEYX9nX1MvtpQ4w8 node index.js
+process.on('SIGTERM', () => server.close());
 
